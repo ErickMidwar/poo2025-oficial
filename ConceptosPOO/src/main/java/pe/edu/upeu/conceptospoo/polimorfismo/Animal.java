f09a817b98327ec4f8ef5db2c6fb99fb14d1b683
ErickMidwar <erickmidwar88@gmail.com>
2025-08-26 10:24:11 -0500

    Add files via upload

---
+++ ConceptosPOO/src/main/java/pe/edu/upeu/conceptospoo/polimorfismo/Animal.java
@@ -0,0 +1,7 @@
+package pe.edu.upeu.conceptospoo.polimorfismo;
+
+public class Animal {
+    void sonidoanimal(){
+        System.out.println("el animla hace un tipo de sonido ");
+    }
+}
---
+++ ConceptosPOO/src/main/java/pe/edu/upeu/conceptospoo/polimorfismo_abtrack/main.java
@@ -0,0 +1,9 @@
+package pe.edu.upeu.conceptospoo.polimorfismo_abtrack;
+
+public class main {
+    public static void main (String[] args) {
+        loro l = new loro();
+        l.comer();
+        l.dormir();
+    }
+}
